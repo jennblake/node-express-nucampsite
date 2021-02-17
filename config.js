59ce497f1ee5eb1463f0e87f4d8e66b480b58d1d
Jenn Blake <jennrblake@gmail.com>
2021-02-17 09:19:10 -0600

    Added Facebook OAuth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'facebook': {
+        clientId: '3726600967424639',
+        clientSecret: 'a480bd94d572b046ef3f7794ca299e7e'
+    }
 }
